d33b03ce2af79a441f4bb3cb9e537380e17c265c
vidit <vidit@vidit-ThinkPad-T430>
2017-12-26 18:26:22 +0530

    FlatMapToPair comments Explanation

--- JavaSparkExamples/src/main/java/com/spark/transform/RDDTransformations.java
+++ JavaSparkExamples/src/main/java/com/spark/transform/RDDTransformations.java
@@ -8,6 +8,7 @@ import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.rdd.RDD;
 
 import scala.Tuple2;
 
@@ -31,8 +32,19 @@ public class RDDTransformations {
 		
 		
 		System.out.println("******************** FlatMap to Pair **********************");
+		
+		/*
+		 * JavaRDD is already a Stream. You can't apply rdd.stream(). In case of FlatMap
+		 * FlatMapToPair, The operation is performed on JavaRDD So there is need to
+		 * convert first Array[] or List into stream to perform map() function.
+		 * FlatMap and FlatMapToPair returns  "java.utils.iterator"
+		 * Arrays.asList ---> RDD ---> flatMapToPair(splitting strings in list from
+		 * RDD)---> Converted into Stream() --> to Apply map(mapping into key value pair
+		 * tuple)--> iterate() over each element to convert into key value tuple.
+		 */
 		JavaRDD<String> stringRDD = sc.parallelize(Arrays.asList("This","is an","example","of","FlatMapToPair"));
-		JavaPairRDD<String,Integer> rdd = stringRDD.flatMapToPair(s->	Arrays.asList(s.split("	")).stream().map(word -> new Tuple2<String, Integer>(word, word.length())).iterator());
+		JavaPairRDD<String,Integer> rdd = stringRDD.flatMapToPair(s->Arrays.asList(s.split(" ")).stream().map(word -> new Tuple2<String, Integer>(word, word.length())).iterator());
+
 		System.out.println(rdd.collect());
 
 		
